6560d8d4941fc60ddeec94545532f898fbd7d8de
Dan Rue <dan.rue@linaro.org>
2017-10-19 12:25:22 -0500

    Fix tests - allow job_id to be int or str

--- engine.py
+++ engine.py
@@ -72,7 +72,7 @@ class lavaTriage(object):
 
 
 def main(job_id):
-    assert job_id.isdigit(), "job_id must be a number"
+    assert str(job_id).isdigit(), "job_id must be a number"
     with open("rules.yaml", 'r') as f:
         rule_file_content = yaml.load(f)
 
@@ -89,7 +89,8 @@ def main(job_id):
 
 if __name__ == '__main__':
     job_id = sys.argv[1]
-    assert job_id.isdigit(), "Usage: {} (lava job number)".format(sys.argv[0])
+    assert str(job_id).isdigit(), (
+        "Usage: {} (lava job number)".format(sys.argv[0]))
     engine, matching_rules = main(job_id)
 
     if matching_rules:
